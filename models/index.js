e8f03e7753db1b36e964729e1d120a8f11bf6a4e
Owais Islam <owaisislam@gmail.com>
2020-10-26 20:21:29 -0500

    added index.js for models

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./User');
+const Post = require('./Post');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+
+module.exports = {
+    User,
+    Post
+};
