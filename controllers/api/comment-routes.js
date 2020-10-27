8c6ba105e1da98034cd4554d63e699f5f0a6b9ee
Owais Islam <owaisislam@gmail.com>
2020-10-26 22:15:31 -0500

    updated comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,4 +5,52 @@ const {
     Comment
 } = require('../../models');
 
+
+//Get all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Create a post
+router.post("/", (req, res) => {
+    Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.body.user_id,
+        })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+//Delete a post
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'No post found with this id'
+                });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
 module.exports = router;
