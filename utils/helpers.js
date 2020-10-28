e300477195e801a6b39a2d296c7b0355d0d154c7
Owais Islam <owaisislam@gmail.com>
2020-10-27 20:06:55 -0500

    added helper function formatDate

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
+
+
+module.exports = {
+    formatDate
+}
