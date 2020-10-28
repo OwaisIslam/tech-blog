ffdc50d1f68139421e8b6a684bea1c2200db08ef
Owais Islam <owaisislam@gmail.com>
2020-10-27 19:56:46 -0500

    started homepage layout

---
+++ controllers/home-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {
+    User,
+    Post,
+    Comment
+} = require('../models');
+
+
+router.get('/', (req, res) => {
+    Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
+
+            res.render('homepage', {
+                posts
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+module.exports = router;
