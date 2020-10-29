f3ac7801c73726b89ed0f65c16213fa678c4da55
Owais Islam <owaisislam@gmail.com>
2020-10-28 20:04:08 -0500

    fixed login check

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -82,7 +82,8 @@ router.post('/', (req, res) => {
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
-            email: req.body.email
+            email: req.body.email,
+            password: req.body.password
         }
     }).then(dbUserData => {
         if (!dbUserData) {
@@ -113,27 +114,6 @@ router.post('/login', (req, res) => {
         });
     });
 });
-
-
-// router.post('/login', (req, res) => {
-//     User.findOne({
-//             where: {
-//                 email: req.body.email
-//             }
-//         })
-//         .then(dbUserData => {
-//             if (!dbUserData) {
-//                 res.status(400).json({
-//                     message: 'No user with that email address!'
-//                 });
-//                 return;
-//             }
-//             res.json({
-//                 user: dbUserData,
-//                 message: 'You are now logged in!'
-//             });
-//         });
-// });
 
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
