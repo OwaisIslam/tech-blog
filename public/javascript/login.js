ca37949c35a419eb143df16fd60ffd4a30f4f236
Owais Islam <owaisislam@gmail.com>
2020-10-29 00:18:02 -0500

    updated login route

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,7 +19,7 @@ async function signupFormHandler(event) {
         });
 
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
